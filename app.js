cbba4e0ebaba50159dafb302ee53af5257486325
Parth Pandya <parth.pandya93@gmail.com>
2020-09-08 13:22:56 +0530

    some Javascript Added

---
+++ app.js
@@ -0,0 +1,77 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(usersChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore; 
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(usersChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}.  You Win!`;
+}
+
+function lose(){
+    console.log("LOST");
+}
+
+function draw(){
+    console.log("DRAW");
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case"rs":
+        case"pr":
+        case"sp":
+        win(userChoice, computerChoice);
+            break;
+        case"rp":
+        case"ps":
+        case"sr":
+        lose(userChoice, computerChoice);
+            break;
+        case"rs":
+        case"rs":
+        case"rs":
+        draw(userChoice, computerChoice);
+            break;
+    }
+    
+}
+
+
+function main(){ 
+        rock_div.addEventListener('click', function(){
+        game("r");
+    })
+
+        paper_div.addEventListener('click', function(){
+        game("p");
+    })
+
+        scissors_div.addEventListener('click', function(){
+        game("s");
+    })
+}
+
+main();
